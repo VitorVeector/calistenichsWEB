1f13587246bf313f400b64d0f46f0761b7b0b498
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2021-05-18 14:28:05 -0300

    First commit
    .

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btnToggle')
+
+function btnToggle(){
+    nav = document.querySelector('nav')
+    nav.classList.toggle('active')
+    
+}
+
+btn.onclick = btnToggle
+
+
+
+
+
+
